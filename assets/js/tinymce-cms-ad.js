eef1cb3f8da33c3357189a4a535ae26e643c3ee1
Jonathan Stegall <jstegall@minnpost.com>
2018-09-04 18:31:14 -0500

    fix tinymce plugin with the shortcodes

--- assets/js/tinymce-cms-ad.js
+++ assets/js/tinymce-cms-ad.js
@@ -1,85 +1,87 @@
 (function() {
-    tinymce.PluginManager.add( 'cms_ad', function( editor, url ) {
-    	var shortcode = /\[([^:]+):([^:\]]+)\]/g;
+	tinymce.create( 'tinymce.plugins.cms_ad', {
 
-    	//helper functions
-        function getAttr(s, n) {
-            n = new RegExp(n + '=\"([^\"]+)\"', 'g').exec(s);
-            return n ?  window.decodeURIComponent(n[1]) : '';
-        };
+		init : function( ed, url ) {
+			var t = this;
 
-		function ifShortcode( content ) {
-			return content.search( shortcode ) !== -1;
-		}
+			t.url = url;
 
-		function replaceShortcodes( content ) {
-			return content.replace( shortcode, function( match, p1, p2 ) {
-				return html( match, p1, p2 );
+			//replace shortcode before editor content set
+			ed.onBeforeSetContent.add( function( ed, o ) {
+				o.content = t._do_cms_ad( o.content );
 			});
-		}
 
-		function restoreShortcodes( content ) {
-			return content.replace( /(?:<p(?: [^>]+)?>)*(<img [^>]+>)(?:<\/p>)*/g, function( match, image ) {
-            	var shortcode = getAttr( image, 'data-shortcode' );
-            	if ( shortcode ) {
-            		var type = getAttr( image, 'data-shortcode-type' );
-            		return '[' + shortcode + ':' + type + ']';
-            	}
-            	return match;
-            });
-		}
-
-		function html( data, shortcode, type ) {
-			var markup = '<p><img src="' + url + '/../img/' + shortcode + '.png"  class="mceItem mceAdShortcode mceAdShortcode' + type + '" ' + 'data-shortcode="' + shortcode + '" data-shortcode-type="' + type + '" data-mce-resize="false" data-mce-placeholder="1"></p>';
-        	return markup;
-    	}
-
-		editor.on( 'BeforeSetContent', function( event ) {
-			// No shortcodes in content, return.
-			if ( ! ifShortcode( event.content ) ) {
-				return;
-			}
-			event.content = replaceShortcodes( event.content );
-		});
-
-		// Display gallery, audio or video instead of img in the element path
-		editor.on( 'ResolveName', function( event ) {
-			var dom = editor.dom, node = event.target;
-			if ( node.nodeName === 'IMG' && dom.getAttrib( node, 'data-shortcode' ) ) {
-				if ( dom.hasClass( node, 'mceAdShortcode' ) ) {
-					event.name = 'cms_ad';
+			//replace shortcode as its inserted into editor (which uses the exec command)
+			ed.onExecCommand.add( function( ed, cmd ) {
+			    if ( 'mceInsertContent' === cmd ) {
+					tinyMCE.activeEditor.setContent( t._do_cms_ad( tinyMCE.activeEditor.getContent() ) );
 				}
-			}
-		});
+			});
 
-		editor.on( 'PostProcess', function( event ) {
-			if ( event.get ) {
-				event.content = restoreShortcodes( event.content );
-			}
-		});
+			//replace the image back to shortcode on save
+			ed.onPostProcess.add( function( ed, o ) {
+				if ( o.get )
+					o.content = t._get_cms_ad( o.content );
+			});
 
-        // Add Ad to Visual Editor Toolbar
-        editor.addButton('cms_ad', {
-            title: 'Insert Ad Shortcode',
-            cmd: 'cms_ad',
-            image: url + '/../img/tinymce-icon.png'
-        });
+			// button to add ads
+			ed.addCommand( 'cms_ad', function() {
+			    // Ask the user to enter an ad code
+			    var result = prompt( 'Enter the ad code to insert' );
+			    if ( ! result ) {
+			        // User cancelled - exit
+			        return;
+			    }
+			    if ( 0 === result.length ) {
+			        // User didn't enter a code
+			        return;
+			    }
+			    // Insert selected text back into editor as a cms_ad shortcode
+			    ed.execCommand( 'mceReplaceContent', false, '[cms_ad:' + result + ']' );
+			});
 
-        // Add Command when Button Clicked
-		editor.addCommand( 'cms_ad', function() {
-		    // Ask the user to enter an ad code
-		    var result = prompt('Enter the ad code to insert');
-		    if ( ! result ) {
-		        // User cancelled - exit
-		        return;
-		    }
-		    if ( 0 === result.length ) {
-		        // User didn't enter a code
-		        return;
-		    }
-		    // Insert selected text back into editor as a cms_ad shortcode
-		    editor.execCommand( 'mceReplaceContent', false, '[cms_ad:' + result + ']' );
-		});
+			// Add Ad to Visual Editor Toolbar
+	        ed.addButton('cms_ad', {
+	            title: 'Insert Ad Shortcode',
+	            cmd: 'cms_ad',
+	            image: url + '/../img/tinymce-icon.png'
+	        });
 
+		},
+
+		_do_cms_ad : function( co ) {
+			return co.replace(/\[([cms_ad:]+):([^:\]]+)\]/g, function( a, b, c ) {
+				return '<img src="/wp-content/plugins/appnexus-acm-provider/assets/img/' + b + '.png" class="mceItem mceAdShortcode mceAdShortcode' + tinymce.DOM.encode( c ) + '" alt="' + tinymce.DOM.encode( b ) + ':' + tinymce.DOM.encode( c ) + '" data-shortcode="' + tinymce.DOM.encode( b ) + '" data-shortcode-type="' + tinymce.DOM.encode( c ) + '" data-mce-resize="false" data-mce-placeholder="1">';
+			});
+		},
+
+		_get_cms_ad : function( co ) {
+
+			function getAttr( s, n ) {
+				n = new RegExp( n + '=\"([^\"]+)\"', 'g' ).exec( s );
+				return n ? tinymce.DOM.decode( n[1] ) : '';
+			};
+
+			return co.replace( /(?:<p[^>]*>)*(<img[^>]+>)(?:<\/p>)*/g, function( a, im ) {
+				var cls = tinymce.trim( getAttr( im, 'class' ) );
+				var shortcode = tinymce.trim( getAttr( im, 'data-shortcode' ) );
+				var type = tinymce.trim( getAttr( im, 'data-shortcode-type' ) );
+				if ( -1 !== cls.indexOf( 'mceAdShortcode' ) && '' !== shortcode && '' !== type ) {
+					return '[' + shortcode + ':' + type + ']';
+				}
+				return a;
+			});
+		},
+
+		getInfo : function() {
+			return {
+				longname : 'cms_ad shortcode replace',
+				author : 'MinnPost',
+				infourl : '',
+				version : "1.0"
+			};
+		}
 	});
+
+	tinymce.PluginManager.add( 'cms_ad', tinymce.plugins.cms_ad );
 })();
